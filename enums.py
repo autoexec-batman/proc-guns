f858fa3157120ca59887be6a273106bb3506cb7c
autoexec-batman <3104219+autoexec-batman@users.noreply.github.com>
2017-08-15 17:44:04 -0400

    basically getting this thing off the ground

---
+++ enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+class Quality(Enum):
+    BAD = 1
+    NORMAL = 2
+    GOOD = 3
+    GREAT = 4
+    SUPER = 5
+    
+class Manufacturer(Enum):
+    ANTILLES = 1
+    BEUS = 2
+    MISHAK = 3
+    OLYMPION = 4
+    SYNDEX = 5
