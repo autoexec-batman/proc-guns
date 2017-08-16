41cedb31466c7e2a9e7a7d0d43449f567633080f
autoexec-batman <3104219+autoexec-batman@users.noreply.github.com>
2017-08-16 16:20:20 -0400

    prettier print on weapon card

--- gun-runner.py
+++ gun-runner.py
@@ -2,7 +2,7 @@ from gun import Gun
 
 chome = Gun()
 
-print (chome.damage)
-print (chome.magazine_size)
-print (chome.fire_rate)
-print (chome.reload_speed)
+print ("Damage per bullet: ", chome.damage)
+print ("Bullets per magazine: ", chome.magazine_size)
+print ("Bullets fired per second: ", chome.fire_rate)
+print ("Seconds per magazine change :", chome.reload_speed)
