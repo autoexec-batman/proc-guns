74c0f7c8a43c841a2cb3573dc792ccdb56a5dbd8
autoexec-batman <3104219+autoexec-batman@users.noreply.github.com>
2017-08-17 17:39:59 -0400

    put manufacturers into their own class

---
+++ proc_guns/manufacturer.py
@@ -0,0 +1,5 @@
+class Manufacturer:
+    def __init__(self, name, quality_list, modifier_list)
+        self.name = name
+        self.qualities = quality_list
+        self.modifiers = modifier_list
