742f0fe333899cf9e81f96320c8c4e26dc2a870a
autoexec-batman <3104219+autoexec-batman@users.noreply.github.com>
2017-08-23 20:38:48 -0400

    making quality generated within manufacturer

--- proc_guns/gun-runner.py
+++ proc_guns/gun-runner.py
@@ -14,13 +14,12 @@ random.seed()
 manufacturer_id = (random.randint(0,len(makers) -1))
 manufacturer = makers[manufacturer_id]
 quality_id = enums.Quality(random.randint(1,5)).name
-quality = makers[manufacturer_id]['qualities'][quality_id]
 
-chome = Gun(BASE_BULLET_DAMAGE, BASE_MAGAZINE_SIZE, BASE_FIRE_RATE, BASE_RELOAD_SPEED, quality, manufacturer)
+chome = Gun(BASE_BULLET_DAMAGE, BASE_MAGAZINE_SIZE, BASE_FIRE_RATE, BASE_RELOAD_SPEED, manufacturer)
 
 print ("Damage per bullet: ", chome.damage)
 print ("Bullets per magazine: ", chome.magazine_size)
 print ("Bullets fired per second: ", chome.fire_rate)
 print ("Seconds per magazine change :", chome.reload_speed)
 print ("Selected Manufacturer: ", chome.manufacturer["name"])
-print ("Selected Quality: ", chome.quality)
+print ("Selected Quality: ", chome.manufacturer["qualities"][quality_id])
--- proc_guns/gun.py
+++ proc_guns/gun.py
@@ -1,9 +1,8 @@
 class Gun:
     
-    def __init__(self, bullet_damage, magazine_size, fire_rate, reload_speed, quality, manufacturer):  
+    def __init__(self, bullet_damage, magazine_size, fire_rate, reload_speed, manufacturer):  
         self.damage = bullet_damage
         self.magazine_size = magazine_size
         self.fire_rate = fire_rate
         self.reload_speed = reload_speed
-        self.quality = quality
         self.manufacturer = manufacturer
