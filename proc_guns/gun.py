7638a74e5a02411494a1c597faf7b8b27c66d389
autoexec-batman <3104219+autoexec-batman@users.noreply.github.com>
2017-08-23 21:34:07 -0400

    making damage and mag_size ints

--- proc_guns/gun.py
+++ proc_guns/gun.py
@@ -2,8 +2,8 @@ class Gun:
     
     def __init__(self, bullet_damage, magazine_size, fire_rate, reload_time, manufacturer):  
         self.manufacturer = manufacturer
-        self.damage = bullet_damage * manufacturer['modifiers']['damage']
-        self.magazine_size = magazine_size * manufacturer['modifiers']['mag_size']
+        self.damage = int(bullet_damage * manufacturer['modifiers']['damage'])
+        self.magazine_size = int(magazine_size * manufacturer['modifiers']['mag_size'])
         self.fire_rate = fire_rate * manufacturer['modifiers']['fire_rate']
         self.reload_time = reload_time * manufacturer['modifiers']['reload_time']
         
