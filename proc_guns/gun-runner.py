7dfd378c3008bd3c8d1c20e0badd834feeafe01a
autoexec-batman <3104219+autoexec-batman@users.noreply.github.com>
2017-08-27 19:50:35 -0400

    standardize on name, add weighted_choice function

--- proc_guns/gun-runner.py
+++ proc_guns/gun-runner.py
@@ -2,16 +2,27 @@ from gun import Gun
 import random
 import yaml
 
+def weighted_choice(list):
+    weighted_list = []
+    for weighted_item in list:
+        for i in range(weighted_item["weight"]):
+            weighted_list.append(weighted_item)
+    #print (weighted_list)
+    return random.choice(weighted_list)
+    
+    
 makers = yaml.load(open("makers.yml"))
 guntypes = yaml.load(open("guntypes.yml"))
 qualities = yaml.load(open("qualities.yml"))
 
+weighted_choice(qualities)
+
 manufacturer = random.choice(makers)
 guntype = random.choice(guntypes)
-quality = random.choice(qualities)
+quality = weighted_choice(qualities)
 
 chome = Gun(guntype, manufacturer, quality)
-display_name = "{0} {1} {2}".format((chome.manufacturer['qualities'][chome.quality["tier"]]), chome.manufacturer["name"], chome.guntype["name"])
+display_name = "{0} {1} {2}".format((chome.manufacturer['qualities'][chome.quality["name"]]), chome.manufacturer["name"], chome.guntype["name"])
 
 print (display_name)
 print ("Damage per bullet: ", chome.damage)
