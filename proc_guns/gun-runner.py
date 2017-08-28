bc2ca7301762a7894de153492a89617ef9ea4592
autoexec-batman <3104219+autoexec-batman@users.noreply.github.com>
2017-08-27 21:16:58 -0400

    added more data to weapon card

--- proc_guns/gun-runner.py
+++ proc_guns/gun-runner.py
@@ -20,11 +20,15 @@ guntype = random.choice(guntypes)
 quality = weighted_choice(qualities)
 
 chome = Gun(guntype, manufacturer, quality)
+
 display_name = "{0} {1} {2}".format((chome.manufacturer['qualities'][chome.quality["name"]]), chome.manufacturer["name"], chome.guntype["name"])
 nice_damage = chome.damage + (chome.damage * chome.nice_multiplier)
+expected_damage = (chome.damage * (1 - chome.nice_chance)) + (nice_damage * chome.nice_multiplier)
+time_to_magdump = chome.magazine_size / chome.fire_rate
+damage_per_mag = expected_damage * chome.magazine_size
+DPS_per_mag = damage_per_mag / time_to_magdump
 
-
-print
+print ("")
 print (display_name)
 print ("Damage per bullet: ", chome.damage)
 print ("Bullets per magazine: ", chome.magazine_size)
@@ -33,3 +37,6 @@ print ("Seconds per magazine change :", chome.reload_time)
 print ("Chance of Nice! shot: ", chome.nice_chance)
 print ("Nice! shot damage multiplier: ", chome.nice_multiplier)
 print ("Nice! shot damage: ", nice_damage)
+print ("")
+print ("Seconds to magdump: ", time_to_magdump)
+print ("Average DPS per magdump: ", DPS_per_mag)
