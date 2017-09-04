507f9e09c854d1a3abf2ebc5b1750295b772fd54
autoexec-batman <31048640+autoexec-batman@users.noreply.github.com>
2017-09-04 14:46:05 -0400

    built more affixes, expanded affix selection routine

--- proc_guns/affix-select.py
+++ proc_guns/affix-select.py
@@ -2,3 +2,13 @@ import yaml
 import random
 
 barrels = yaml.load(open("affix_barrel.yml"))
+print (barrels)
+print ("")
+
+affix_class = random.choice(barrels)
+print (affix_class)
+print("")
+
+affix = random.choice(affix_class['tiers'])
+print (affix)
+print ("")
