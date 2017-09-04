dbaa38b4de3009bb5dca5a93c6183c9cdfc87373
autoexec-batman <31048640+autoexec-batman@users.noreply.github.com>
2017-09-04 16:10:39 -0400

    added sights

--- proc_guns/affix-select.py
+++ proc_guns/affix-select.py
@@ -2,13 +2,19 @@ import yaml
 import random
 
 barrels = yaml.load(open("affix_barrel.yml"))
+sights = yaml.load(open("affix_sight.yml"))
 print (barrels)
+print(sights)
 print ("")
 
-affix_class = random.choice(barrels)
-print (affix_class)
+barrel_class = random.choice(barrels)
+sight_class = random.choice(sights)
+print (barrel_class)
+print(sight_class)
 print("")
 
-affix = random.choice(affix_class['tiers'])
-print (affix)
+barrel_affix = random.choice(barrel_class['tiers'])
+sight_affix = random.choice(sight_class['tiers'])
+print (barrel_affix)
+print (sight_affix)
 print ("")
