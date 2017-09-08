5734a4422651c6c88c3073bbdf8a6cb4160bbeb4
autoexec-batman <31048640+autoexec-batman@users.noreply.github.com>
2017-09-08 15:41:19 -0400

    working on getting affixes working

--- proc_guns/gun.py
+++ proc_guns/gun.py
@@ -1,3 +1,4 @@
+import random
 class Gun:
     
     def __init__(self, guntype, manufacturer, quality):  
@@ -12,7 +13,17 @@ class Gun:
         base_nice_chance = guntype['base_stats']['nice_chance'] * manufacturer['modifiers']['nice_chance']
         base_nice_multiplier = guntype['base_stats']['nice_multiplier'] * manufacturer['modifiers']['nice_multiplier']
 		
-        self.damage = int(base_damage)
+        affixes = set()
+		available_parts = ['barrel', 'sight', 'magazine']
+		random.shuffle[available_parts]
+		parts = set()
+		
+		part_count = random.randint(0,3)
+		
+		
+		
+		
+		self.damage = int(base_damage)
         self.magazine_size = int(base_magazine_size)
         self.fire_rate = base_fire_rate 
         self.reload_time = base_reload_time
